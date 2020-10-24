d2c67b209e24bbe33ef024bd349fa7a4ea2e8905
szkliUj <pawel.szkliniarz@student.uj.edu.pl>
2020-10-24 23:17:51 +0200

    poprawa 2 dodanie 1

--- zadania-1/cw1/cw1.py
+++ zadania-1/cw1/cw1.py
@@ -1,3 +1,21 @@
-x  = int
-input(x)
-print("przyjety int")
+pyramid1 = str()
+
+n = int(input("Podaj nieparzystą liczbę: "))
+
+levels = int(n / 2)
+
+for x in range(n):
+    pyramid1 += "*"
+
+#wyśrodkuje tekst względem n znaków
+print(pyramid1.center(n))
+
+while levels > 0:
+    pyramid2 = pyramid1[1:len(pyramid1) - 1]
+
+    pyramid1 = pyramid2
+
+    print(pyramid1.center(n))
+
+    levels -= 1
+
--- zadania-1/cw2/cw2.py
+++ zadania-1/cw2/cw2.py
@@ -1,6 +1,6 @@
-#continue if 3k
 for x in range(31):
-    if x/3.0 - int(x/3.0) == 0:
+    if x%3 == 0:
         continue
     print(x)
+	
 
